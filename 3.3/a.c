b0cbc7a5038fff303b8d1df89a47113cace74e91
rosarie <rosariosanchezsampietro@gmail.com>
2023-04-07 12:44:30 -0300

    Ejercicio 3.3.a

---
+++ 3.3/a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int entero = 10;
+    char letra = 'R';
+
+    printf("Mostrando un número entero: %i\n", entero);
+    printf("Mostrando un caracter: %c \n", letra);
+    printf("Fin del programa 3.3.a :)\n");
+    return 0;
+}
